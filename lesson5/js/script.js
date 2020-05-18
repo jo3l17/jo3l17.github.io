6ccf524da27a617e0f8a3d6316ff75c9c6164023
jo3l17 <47827494+jo3l17@users.noreply.github.com>
2020-05-18 17:33:08 -0500

    W05 assignmet Preston new Page

---
+++ lesson5/js/script.js
@@ -0,0 +1,36 @@
+var today = new Date()
+var days =['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+var months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+var date = `${days[today.getDay()]}, ${today.getDate()} ${months[today.getMonth()]} ${today.getFullYear()}`
+document.getElementById('date').innerHTML = date
+const hambutton = document.getElementById('menu_button');
+const mainnav = document.getElementById('main_nav');
+const forecastDays = document.getElementById("forecastDays");
+const message = document.getElementById("message");
+var forecastArrayDays=['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
+
+hambutton.addEventListener('click', () => {
+    if(mainnav.classList.value=='navLinks'){
+        hambutton.innerHTML="&#x2715"
+    }else{
+        hambutton.innerHTML="&#9776; Menu"
+    }
+    mainnav.classList.toggle('opened');
+    
+}, false);
+var counter = today.getDay()
+for (let i = 0; i < 5; i++) {
+    var cell = document.createElement('td')
+    if(counter==6){
+        counter=0
+    }else{
+        counter++
+    }
+    cell.innerHTML=forecastArrayDays[counter]
+    forecastDays.appendChild(cell)
+}
+if(counter==5){
+    message.style.display='block'
+}else[
+    message.style.display='none'
+]
