4380a360c9b4e5c94f589d08ba6057953a9ff044
jo3l17 <47827494+jo3l17@users.noreply.github.com>
2020-06-01 14:36:09 -0500

    Update lazyload.js

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -7,7 +7,7 @@ const loadImages = (image) => {
 
 const imgOptions = {
     threshold: 0,
-    rootMargin: "0px 0px 400px 0px"
+    rootMargin: "0px 0px 100px 0px"
 };
 if ('IntersectionObserver' in window) {
     const imgObserver = new IntersectionObserver((entries, imgObserver) => {
